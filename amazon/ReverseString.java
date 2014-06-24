67a4ce8c908b1d0cae3ed3f266fd48e571140a11
rioshen <rioxshen@gmail.com>
2014-06-24 15:44:15 -0400

    Reverse a string list.
    Reverse a string list in place, not more additional space required.

---
+++ amazon/ReverseString.java
@@ -0,0 +1,34 @@
+/**
+ * Problem: Reverse a string list.
+ */
+
+public class ReverseString {
+    /**
+     * Solution:
+     * Swap two elements in the list based on (1, n - 1) pair. Use a temporaty variable
+     * to store the element.
+     *
+     * Analysis:
+     * Time Complexity
+     */
+    public static String reverse(String str) {
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+
+        int pos = 0;
+        char[] reversed = new char[str.length()];
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed[pos++]  = str.charAt(i);
+        }
+
+        return new String(reversed);
+    }
+
+    public static void main(String[] args) {
+        String str = "abcde";
+        System.out.println(reverse(str));
+        str = "abcdef";
+        System.out.println(reverse(str));
+    }
+}
